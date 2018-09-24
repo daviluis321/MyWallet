68ae76e2289e8f53851795f0d0ea39a15e1fb17e
Davi Luis de Oliveira <daviluis323@gmail.com>
2018-09-24 08:13:54 -0300

    Modificando o Banco

--- mywallet/core/models.py
+++ mywallet/core/models.py
@@ -7,28 +7,30 @@ class Usuario(models.Model):
 	cpf   = models.CharField(max_length=11, unique=True)
 	email = models.EmailField(max_length=80, unique=True)
 
+class TipoInvestimento(models.Model):
+	tipo = models.CharField(max_length=30)
+
+class Moeda(models.Model):
+	nome = models.CharField(max_length=10)
+
 class Investimento(models.Model):
 	id_usuario 			= models.ForeignKey(Usuario, on_delete=models.CASCADE)
 	id_tipoInvestimento = models.ForeignKey(TipoInvestimento, on_delete=models.CASCADE)
-	id_moeda			= models.ForeignKey(Moeda, on_delete)
+	id_moeda			= models.ForeignKey(Moeda, on_delete=models.CASCADE)
 	capital    			= models.FloatField()
 	criacao    			= models.DateField(auto_now=False, auto_now_add=False)
 
-class TipoInvestimento(models.Model):
+class TipoDespesa(models.Model):
 	tipo = models.CharField(max_length=30)
 
 class Despesa(models.Model):
 	id_usuario	   = models.ForeignKey(Usuario, on_delete=models.CASCADE)
 	id_tipoDespesa = models.ForeignKey(TipoDespesa, on_delete=models.CASCADE)
-	id_moeda	   = models.ForeignKey(Moeda, on_delete)
+	id_moeda	   = models.ForeignKey(Moeda, on_delete=models.CASCADE)
 	custo     	   = models.FloatField()
 	criacao    	   = models.DateField(auto_now=False, auto_now_add=False)
 
-class TipoDespesa(models.Model):
-	tipo = models.CharField(max_length=30)
 
-class Moeda(models.Model):
-	nome = models.CharField(max_length=10)
 
 class Meta(models.Model):
 	id_usuario   = models.ForeignKey(Usuario, on_delete=models.CASCADE)
