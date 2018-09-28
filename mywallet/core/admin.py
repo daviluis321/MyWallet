8f3394b5b0b4821f3428e87608d620ebfdf0e423
Davi Luis de Oliveira <daviluis323@gmail.com>
2018-09-28 10:12:22 -0300

    Eliminando o Profile do Admin

--- mywallet/core/admin.py
+++ mywallet/core/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
 from .models import *
 
-admin.site.register(Profile)
 admin.site.register(TipoInvestimento)
 admin.site.register(Moeda)
 admin.site.register(Investimento)
