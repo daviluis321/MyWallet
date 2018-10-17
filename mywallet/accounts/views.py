c8e828f2d414745b5110aeead772ae2a708f1f49
Davi Luis de Oliveira <daviluis323@gmail.com>
2018-10-17 06:41:56 -0300

    Lógica do Cadastro

--- mywallet/accounts/views.py
+++ mywallet/accounts/views.py
@@ -1,6 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth import login as auth_login
 
 def cadastro(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            auth_login(request, user)
+            return redirect('home')
+    else:
+        form = UserCreationForm()
     return render(request, 'cadastro.html', {'form': form})
