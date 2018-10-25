dd1ac1f9b141a1739eb6e9739c5213053e2ec234
Davi Luis de Oliveira <daviluis323@gmail.com>
2018-10-25 10:49:59 -0300

    Manutenção do campo nome nas models

--- mywallet/core/models.py
+++ mywallet/core/models.py
@@ -12,36 +12,35 @@ class Moeda(models.Model):
 		return self.nome
 
 class Investimento(models.Model):
-	id_usuario 			= models.ForeignKey(User, on_delete=models.CASCADE)
+	nome = models.CharField(max_length=25,unique=True,blank=False)
+	id_usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 	id_tipoInvestimento = models.ForeignKey(TipoInvestimento, on_delete=models.CASCADE,null=True,blank=True)
 	id_moeda = models.ForeignKey(Moeda, on_delete=models.CASCADE,null=True,blank=True)
 	capital = models.FloatField()
 	criacao = models.DateField(auto_now=False, auto_now_add=False)
 
 	def __str__(self):
-		return self.name
+		return self.nome
 
 class TipoDespesa(models.Model):
 	tipo = models.CharField(max_length=30)
 
 	def __str__(self):
-		return self.name
-
+		return self.tipo
 
 class Despesa(models.Model):
-	#id_usuario	   = models.ForeignKey(User, on_delete=models.CASCADE)
-	#id_tipoDespesa = models.ForeignKey(TipoDespesa, on_delete=models.CASCADE)
-	#id_moeda	   = models.ForeignKey(Moeda, on_delete=models.CASCADE)
-	#custo     	   = models.FloatField()
-	#criacao    	   = models.DateField(auto_now=False, auto_now_add=False)
+	nome = models.CharField(max_length=25,unique=True,blank=False)
+	id_usuario = models.ForeignKey(User, on_delete=models.CASCADE,null=True,blank=True)
+	id_tipoDespesa = models.ForeignKey(TipoDespesa, on_delete=models.CASCADE,null=True,blank=True)
+	id_moeda = models.ForeignKey(Moeda, on_delete=models.CASCADE,null=True,blank=True)
+	custo = models.FloatField(null=True,blank=True)
+	criacao = models.DateField(auto_now=False, auto_now_add=False,null=True,blank=True)
 
 	def __str__(self):
-		return self.name
+		return self.nome
 	
-
-
-
 class Meta(models.Model):
+	nome = models.CharField(max_length=25,unique=True,blank=False)
 	id_usuario   = models.ForeignKey(User, on_delete=models.CASCADE)
 	investimento = models.FloatField()
 	despesa      = models.FloatField()
@@ -51,11 +50,14 @@ class Meta(models.Model):
 	descricao	 = models.CharField(max_length=200)
 
 	def __str__(self):
-		return self.name
+		return self.nome
 
 class Fundo(models.Model):
-	id_usuario   	= models.ForeignKey(User, on_delete=models.CASCADE)
+	nome = models.CharField(max_length=25,unique=True,blank=False)
+	id_usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 	id_investimento = models.ForeignKey(Investimento, on_delete=models.CASCADE)
-	id_despesa   	= models.ForeignKey(Despesa, on_delete=models.CASCADE)
-	saldo			= models.FloatField()
-	
+	id_despesa = models.ForeignKey(Despesa, on_delete=models.CASCADE)
+	saldo = models.FloatField()
+
+	def __str__(self):
+		return self.nome
