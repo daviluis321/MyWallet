9bfa167b2eb4bc600e436167f6931974661eb586
Davi Luis de Oliveira <daviluis323@gmail.com>
2018-10-26 07:45:57 -0300

    Add null = true nos Campos

--- mywallet/core/models.py
+++ mywallet/core/models.py
@@ -2,17 +2,17 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class TipoInvestimento(models.Model):
-	tipo = models.CharField(max_length=30)
+	tipo = models.CharField(max_length=30,null=True,blank=True)
 	def __str__(self):
 		return self.tipo
 
 class Moeda(models.Model):
-	nome = models.CharField(max_length=25,unique=True,blank=False)
+	nome = models.CharField(max_length=25,null=True,blank=True)
 	def __str__(self):
 		return self.nome
 
 class Investimento(models.Model):
-	nome = models.CharField(max_length=25,unique=True,blank=False)
+	nome = models.CharField(max_length=25,null=True,blank=True)
 	id_usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 	id_tipoInvestimento = models.ForeignKey(TipoInvestimento, on_delete=models.CASCADE,null=True,blank=True)
 	id_moeda = models.ForeignKey(Moeda, on_delete=models.CASCADE,null=True,blank=True)
@@ -29,7 +29,7 @@ class TipoDespesa(models.Model):
 		return self.tipo
 
 class Despesa(models.Model):
-	nome = models.CharField(max_length=25,unique=True,blank=False)
+	nome = models.CharField(max_length=25,null=True,blank=True)
 	id_usuario = models.ForeignKey(User, on_delete=models.CASCADE,null=True,blank=True)
 	id_tipoDespesa = models.ForeignKey(TipoDespesa, on_delete=models.CASCADE,null=True,blank=True)
 	id_moeda = models.ForeignKey(Moeda, on_delete=models.CASCADE,null=True,blank=True)
@@ -40,20 +40,20 @@ class Despesa(models.Model):
 		return self.nome
 	
 class Meta(models.Model):
-	nome = models.CharField(max_length=25,unique=True,blank=False)
-	id_usuario   = models.ForeignKey(User, on_delete=models.CASCADE)
+	nome = models.CharField(max_length=25,null=True,blank=True)
+	id_usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 	investimento = models.FloatField()
-	despesa      = models.FloatField()
-	criacao		 = models.DateField(auto_now=False, auto_now_add=False)
-	inicio		 = models.DateField(auto_now=False, auto_now_add=False)
-	prazo		 = models.DateField(auto_now=False, auto_now_add=False)
-	descricao	 = models.CharField(max_length=200)
+	despesa = models.FloatField()
+	criacao = models.DateField(auto_now=False, auto_now_add=False)
+	inicio = models.DateField(auto_now=False, auto_now_add=False)
+	prazo  = models.DateField(auto_now=False, auto_now_add=False)
+	descricao = models.CharField(max_length=200)
 
 	def __str__(self):
 		return self.nome
 
 class Fundo(models.Model):
-	nome = models.CharField(max_length=25,unique=True,blank=False)
+	nome = models.CharField(max_length=25,null=True,blank=True)
 	id_usuario = models.ForeignKey(User, on_delete=models.CASCADE)
 	id_investimento = models.ForeignKey(Investimento, on_delete=models.CASCADE)
 	id_despesa = models.ForeignKey(Despesa, on_delete=models.CASCADE)
