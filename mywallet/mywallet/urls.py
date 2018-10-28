1b5c64692cb51af928957ae9fff88f22be72c189
HelbertMonteiro <helbert.monteiro0@gmail.com>
2018-10-28 14:26:17 -0300

    Autenticação com Django

--- mywallet/mywallet/urls.py
+++ mywallet/mywallet/urls.py
@@ -17,10 +17,11 @@ from django.contrib import admin
 from django.urls import path
 from core import views
 from accounts import views as accounts_views
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('',views.home, name='home'),
-    path('login/',views.login, name='login'),
+    path(r'^login/$', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
     path('cadastro/',accounts_views.cadastro,name='cadastro'),
     path('contato/',views.contato, name='contato'),
     path('admin/', admin.site.urls),
