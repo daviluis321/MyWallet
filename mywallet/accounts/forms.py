4e26ae32e97671064312162e8c6e6597b67bc85e
Davi Luis de Oliveira <daviluis323@gmail.com>
2018-11-08 17:55:49 -0300

    ajustes de layout do cadastro

--- mywallet/accounts/forms.py
+++ mywallet/accounts/forms.py
@@ -4,6 +4,11 @@ from django.contrib.auth.models import User
 
 class SignUpForm(UserCreationForm):
     email = forms.CharField(max_length=254, required=True, widget=forms.EmailInput())
+    
     class Meta:
         model = User
         fields = ('username', 'email', 'password1', 'password2')
+        help_texts = {
+            'username': '',
+            'password1': '',
+        }
