e95446fcd089044a1f83f245ea9015ad13e85e87
Davi Luis de Oliveira <daviluis323@gmail.com>
2018-11-12 07:19:02 -0300

    Add logout

--- mywallet/mywallet/settings.py
+++ mywallet/mywallet/settings.py
@@ -123,3 +123,4 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 LOGIN_REDIRECT_URL = 'main'
+LOGOUT_REDIRECT_URL = 'home'
--- mywallet/mywallet/urls.py
+++ mywallet/mywallet/urls.py
@@ -22,6 +22,7 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('',views.home, name='home'),
     path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
     path('cadastro/',accounts_views.cadastro,name='cadastro'),
     path('contato/',views.contato, name='contato'),
     path('admin/', admin.site.urls),
