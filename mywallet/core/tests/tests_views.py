847fda84a45efca8756644dbf5355cd4a2dac93c
Davi Luis de Oliveira <daviluis323@gmail.com>
2018-11-12 14:07:46 -0300

    Organizando os Testes das Views

--- mywallet/core/tests/tests_views.py
+++ mywallet/core/tests/tests_views.py
@@ -1,15 +1,10 @@
 from django.urls import reverse
 from django.test import TestCase
-
-class TestsViewCode(TestCase):
+class HomeTests(TestCase):
     def test_home_view_status_code(self):
         url = reverse('home')
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
-
-   # def test_home_url_resolves_home_view(self):
-    #        view = resolve('/')
-    #        self.assertEquals(view.func, home)
 
     def test_login_view_status_code(self):
         url = reverse('login')
