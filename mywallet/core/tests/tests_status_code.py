6f63438dc4549933bb58695777c629cbd1925819
Davi Luis de Oliveira <daviluis323@gmail.com>
2018-11-14 12:55:34 -0300

    Add Testes de retorno de template

--- mywallet/core/tests/tests_status_code.py
+++ mywallet/core/tests/tests_status_code.py
@@ -1,27 +1,27 @@
 from django.urls import reverse
 from django.test import TestCase
-class CoreTestsView(TestCase):
-    def test_home_view_status_code(self):
+class CoreStatus(TestCase):
+    def test_home_status_code(self):
         url = reverse('home')
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
 
-    def test_login_view_status_code(self):
+    def test_login_status_code(self):
         url = reverse('login')
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
     
-    def test_main_view_status_code(self):
+    def test_main_status_code(self):
         url = reverse('main')
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
 
-    def test_contato_view_status_code(self):
+    def test_contato_status_code(self):
         url = reverse('contato')
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
   
-    def test_quem_somos_view_status_code(self):
+    def test_quem_somos_status_code(self):
         url = reverse('qsomos')
         response = self.client.get(url)
         self.assertEquals(response.status_code, 200)
