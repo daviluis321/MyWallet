d466fd0dfd41265f7c587cabf8758a011efce592
Davi Luis de Oliveira <daviluis323@gmail.com>
2018-11-15 16:52:46 -0300

    Manutenção do erro de cahe

--- mywallet/mywallet/settings.py
+++ mywallet/mywallet/settings.py
@@ -121,6 +121,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
 LOGIN_REDIRECT_URL = 'main'
 LOGOUT_REDIRECT_URL = 'home'
