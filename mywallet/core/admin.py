08a7ab12371628fd6086219b0e61f466e39e0230
brenamaia <brenamaia12@gmail.com>
2018-11-18 14:39:39 -0300

    atualizacaoFinal

---
+++ mywallet/core/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import *
+
+admin.site.register(TipoInvestimento)
+admin.site.register(Moeda)
+admin.site.register(Investimento)
+admin.site.register(TipoDespesa)
+admin.site.register(Despesa)
+admin.site.register(Meta)
+admin.site.register(Fundo)
---
+++ mywallet/mywallet/urls.py
@@ -0,0 +1,35 @@
+"""mywallet URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.1/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from core import views
+from accounts import views as accounts_views
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('',views.home, name='home'),
+    path('login/', auth_views.LoginView.as_view(template_name='login.html'), name='login'),
+    path('logout', auth_views.LogoutView.as_view(), name='logout'),
+    path('cadastro/',accounts_views.cadastro,name='cadastro'),
+    path('contato/',views.contato, name='contato'),
+    path('admin/', admin.site.urls),
+    path('main/',views.main, name='main'),
+    path('investimento/',views.investimento, name='investimento'),
+    path('despesa/',views.despesa, name='despesa'),
+    path('qsomos/',views.qsomos, name = 'qsomos'),
+    path('delete_page/', accounts_views.delete_page, name='delete_page'),
+    path('delete/<username>', accounts_views.delete, name='delete'),
+]
