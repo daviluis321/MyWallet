00d575fb867e0fa27e27c4d21092da6c70998d0a
Davi Luis <daviluis323@gmail.com>
2018-11-20 16:31:50 -0300

    Add Static Storage

--- mywallet/settings.py
+++ mywallet/settings.py
@@ -146,3 +146,7 @@ try:
     from .local_settings import *
 except ImportError:
     pass
+
+# Simplified static file serving.
+# https://warehouse.python.org/project/whitenoise/
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
