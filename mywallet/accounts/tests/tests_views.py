be230a91328c8b509cb34ea014f7ba5ca48213c9
Naara <naaraguria@gmail.com>
2018-11-20 21:40:57 -0300

    Reiniciando e organizando pasta do projeto

---
+++ mywallet/accounts/tests/tests_views.py
@@ -0,0 +1,22 @@
+from django.test import TestCase
+from django.urls import reverse, resolve
+
+
+class  AccountsTestsView(TestCase):
+    def test_cadastro_view_status_code(self):
+        url = reverse('cadastro')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 200)
+
+    def test_logout_view_status_code(self):
+        url = reverse('logout')
+        response = self.client.get(url)
+        self.assertEquals(response.status_code, 302)
+
+    #def test_home_url_resolves_home_view(self):
+    #   view = resolve('/cadastro')
+    #    self.assertEquals(view.cadastro, cadastro)
+
+    #def test_home_url_resolves_home_view(self):
+    #    view = resolve('/cadastro')
+    #   self.assertEquals(User.objects, None)
